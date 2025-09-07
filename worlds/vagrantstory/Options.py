ea8a26fa3532d72dfe5f8b6cedff8cd94ac5e2e9
riezahughes <dan@tantalus-troupe.com>
2025-09-08 01:54:46 +0200

    additional options

--- worlds/vagrantstory/Options.py
+++ worlds/vagrantstory/Options.py
@@ -2,38 +2,49 @@ import typing
 from dataclasses import dataclass
 from Options import Toggle, DefaultOnToggle, Option, Range, Choice, ItemDict, DeathLink, PerGameCommonOptions
 
-class GoalOptions():
+
+class GoalOptions:
     DEFEAT_ANGEL = 1
 
-class ProgressionOptions():
+
+class ProgressionOptions:
     VANILLA = 0
     RANDOM = 1
 
+
 class GuaranteedItemsOption(ItemDict):
     """Guarantees that the specified items will be in the item pool"""
+
     display_name = "Guaranteed Items"
+
 
 class GoalOption(Choice):
     """Lets the user choose the completion goal
     Defeat Final Boss - Beat the boss at the end"""
+
     display_name = "Completion Goal"
     default = GoalOptions.DEFEAT_ANGEL
     option_defeat_final_boss = GoalOptions.DEFEAT_ANGEL
-    
+
+
 class ProgressionOption(Choice):
     """Lets users choose how they wish to progress
     Vanilla - Plays the game like normal
     (Will only do Vanilla for now)"""
+
     display_name = "Game Progression Options"
     default = ProgressionOptions.VANILLA
     option_vanilla = ProgressionOptions.VANILLA
-    
+
+
 class DeathLinkToggle(Toggle):
     """Sets if you want deathlink or not"""
+
     display_name = "Death Link"
     default = 0
     option_true = 1
     option_false = 0
+
 
 class IncludeNewGamePlusItems(Toggle):
     display_name = "Include New Game Plus Items"
@@ -41,15 +52,34 @@ class IncludeNewGamePlusItems(Toggle):
     option_true = 1
     option_false = 0
 
+
+class IncludePuzzleModeChecks(Toggle):
+    display_name = "Include Puzzle Mode Checks"
+    default = 0
+    option_true = 1
+    option_false = 0
+
+
+class IncludeMidBossChecks(Toggle):
+    display_name = "Include Mini Bosses that hold keys/sigils as checks"
+    default = 1
+    option_true = 1
+    options_false = 0
+
+
 class AllDoorsUnlocked(Toggle):
     display_name = "All Doors Unlocked"
     default = 0
     option_true = 1
     option_false = 0
 
+
 @dataclass
 class VagrantStoryOption(PerGameCommonOptions):
     goal: GoalOption
     progression_option: ProgressionOption
+    include_new_game_plus: IncludeNewGamePlusItems
+    include_puzzle_mode_checks: IncludePuzzleModeChecks
+    include_mid_boss_checks: IncludeMidBossChecks
     deathlink: DeathLinkToggle
     guaranteed_items: GuaranteedItemsOption
--- worlds/vagrantstory/__init__.py
+++ worlds/vagrantstory/__init__.py
@@ -11,6 +11,7 @@ from .Items import VagrantStoryItem, VagrantStoryItemCategory, item_dictionary, 
 from .Locations import VagrantStoryLocation, VagrantStoryLocationCategory, VagrantStoryLocationData, location_tables, location_dictionary
 from .Options import VagrantStoryOption, GoalOptions
 
+
 class VagrantStoryWeb(WebWorld):
     bug_report_page = ""
     theme = "stone"
@@ -20,7 +21,7 @@ class VagrantStoryWeb(WebWorld):
         "English",
         "setup_en.md",
         "setup/en",
-        ["RiezaHughes"]
+        ["RiezaHughes"],
     )
 
     tutorials = [setup_en]
@@ -43,11 +44,8 @@ class VagrantStoryWorld(World):
     required_client_version = (0, 5, 0)
     item_name_to_id = VagrantStoryItem.get_name_to_id()
     location_name_to_id = VagrantStoryLocation.get_name_to_id()
-    item_name_groups = {
-    }
+    item_name_groups = {}
     item_descriptions = item_descriptions
-    
-
 
     def __init__(self, multiworld: MultiWorld, player: int):
         super().__init__(multiworld, player)
@@ -56,27 +54,26 @@ class VagrantStoryWorld(World):
         self.main_path_locations = []
         self.enabled_location_categories = set()
 
-
     def generate_early(self):
         self.enabled_location_categories.add(VagrantStoryLocationCategory.PROGRESSION)
-        self.enabled_location_categories.add(VagrantStoryLocationCategory.CHEST)        
+        self.enabled_location_categories.add(VagrantStoryLocationCategory.CHEST)
         self.enabled_location_categories.add(VagrantStoryLocationCategory.GRIMOIRES)
         self.enabled_location_categories.add(VagrantStoryLocationCategory.BOSS)
-        self.enabled_location_categories.add(VagrantStoryLocationCategory.BOSS_PLUS)        
+        self.enabled_location_categories.add(VagrantStoryLocationCategory.BOSS_PLUS)
         self.enabled_location_categories.add(VagrantStoryLocationCategory.SIGIL_UNLOCKS)
-        self.enabled_location_categories.add(VagrantStoryLocationCategory.KEY_UNLOCKS)        
-        self.enabled_location_categories.add(VagrantStoryLocationCategory.ROOD_INVERSE_UNLOCKS)  
+        self.enabled_location_categories.add(VagrantStoryLocationCategory.KEY_UNLOCKS)
+        self.enabled_location_categories.add(VagrantStoryLocationCategory.ROOD_INVERSE_UNLOCKS)
         self.enabled_location_categories.add(VagrantStoryLocationCategory.ABILITY_UNLOCKS)
         self.enabled_location_categories.add(VagrantStoryLocationCategory.BREAK_UNLOCKS)
-        self.enabled_location_categories.add(VagrantStoryLocationCategory.FLOOR_TRAPS)             
+        self.enabled_location_categories.add(VagrantStoryLocationCategory.FLOOR_TRAPS)
         self.enabled_location_categories.add(VagrantStoryLocationCategory.LEVEL_END)
 
     def create_regions(self):
         # Create Regions
         regions: Dict[str, Region] = {}
-        
+
         regions["Menu"] = self.create_region("Menu", [])
-        
+
         list_of_regions = [
             "Prologue",
             "Wine Cellar",
@@ -109,23 +106,22 @@ class VagrantStoryWorld(World):
             "Town Center East",
             "Town Center South",
             "Credits",
-            "Ashley"
+            "Ashley",
         ]
-        
-        # ALTER IF CHANGED BASED ON OPTIONS LIKE SO        
+
+        # ALTER IF CHANGED BASED ON OPTIONS LIKE SO
         # if(self.options.include_ant_hill_in_checks.value == IncludeAntHillInChecksToggle.option_true):
         #     list_of_regions.insert(8, "Ant Hill")
         # else:
         #     location_tables.pop("Ant Hill")
-        
+
         regions.update({region_name: self.create_region(region_name, location_tables[region_name]) for region_name in list_of_regions})
-        
-     
+
         def create_connection(from_region: str, to_region: str):
             connection = Entrance(self.player, f"{from_region} -> {to_region}", regions[from_region])
             regions[from_region].exits.append(connection)
             connection.connect(regions[to_region])
-            
+
         create_connection("Menu", "Ashley")
         create_connection("Menu", "Prologue")
         create_connection("Prologue", "Wine Cellar")
@@ -158,11 +154,11 @@ class VagrantStoryWorld(World):
         create_connection("Snowfly Forest East", "Town Center West")
         create_connection("Town Center West", "Town Center East")
         create_connection("Town Center East", "Town Center South")
-                                                                                                                   
+
     # For each region, add the associated locations retrieved from the corresponding location_table
     def create_region(self, region_name, location_table) -> Region:
         new_region = Region(region_name, self.player, self.multiworld)
-        
+
         for location in location_table:
             # CAN ALTER INDIVIDUAL LOCATIONS TO REMOVE THEM FROM THE POOL HERE
             # if self.options.include_ant_hill_in_checks.value == IncludeAntHillInChecksToggle.option_false and location.name == "Energy Vial: Megwynne Stormbinder - HH":
@@ -176,18 +172,11 @@ class VagrantStoryWorld(World):
                     location.category,
                     location.default_item,
                     self.location_name_to_id[location.name],
-                    new_region
+                    new_region,
                 )
             else:
                 event_item = self.create_item(location.default_item)
-                new_location = VagrantStoryLocation(
-                    self.player,
-                    location.name,
-                    location.category,
-                    location.default_item,
-                    None,
-                    new_region
-                )
+                new_location = VagrantStoryLocation(self.player, location.name, location.category, location.default_item, None, new_region)
                 event_item.code = None
                 # Cast the item to the correct type
                 if isinstance(event_item, VagrantStoryItem):
@@ -195,28 +184,26 @@ class VagrantStoryWorld(World):
             # Uncomment to print all locations
             # print(f"{self.location_name_to_id[location.name]}: {location.name}")
             new_region.locations.append(new_location)
-            
+
         self.multiworld.regions.append(new_region)
         return new_region
 
-
     def create_items(self):
-        
-        randomized_location_count = 0 
+        randomized_location_count = 0
         for location in self.multiworld.get_locations(self.player):
             if not location.locked and location.address is not None:
                 randomized_location_count += 1
-        
+
         print(f"Requesting itempool size for randomized locations: {randomized_location_count}")
-        
+
         # Call BuildItemPool to get a list of item NAMES (strings)
         item_names_to_add = BuildItemPool(randomized_location_count, self.options)
-        
+
         generated_items: List[Item] = []
         for item_name in item_names_to_add:
             new_item = self.create_item(item_name)
             generated_items.append(new_item)
-            
+
         print(f"Created item pool size: {len(generated_items)}")
 
         # Add the generated VagrantStoryItem objects to the multiworld's item pool
@@ -224,7 +211,7 @@ class VagrantStoryWorld(World):
 
     def create_item(self, name: str) -> Item:
         item_data = item_dictionary.get(name)
-        
+
         if not item_data:
             # Fallback for unknown items. This indicates a data inconsistency.
             print(f"Warning: Attempted to create unknown item: {name}. Falling back to filler.")
@@ -233,82 +220,85 @@ class VagrantStoryWorld(World):
         # Determine the Archipelago ItemClassification based on VagrantStoryItemData.
         item_classification: ItemClassification
 
-        if item_data.progression or item_data.category == VagrantStoryItemCategory.KEYS or item_data.category == VagrantStoryItemCategory.SIGILS:
+        if (
+            item_data.progression
+            or item_data.category == VagrantStoryItemCategory.KEYS
+            or item_data.category == VagrantStoryItemCategory.SIGILS
+        ):
             item_classification = ItemClassification.progression
-        elif item_data.category == VagrantStoryItemCategory.GRIMOIRE or item_data.category == VagrantStoryItemCategory.CHAIN_ABILITY or item_data.category == VagrantStoryItemCategory.DEFENCE_ABILITY:
+        elif (
+            item_data.category == VagrantStoryItemCategory.GRIMOIRE
+            or item_data.category == VagrantStoryItemCategory.CHAIN_ABILITY
+            or item_data.category == VagrantStoryItemCategory.DEFENCE_ABILITY
+        ):
             item_classification = ItemClassification.useful
-        else: # Default for FILLER or other categories not explicitly useful/progression
+        else:  # Default for FILLER or other categories not explicitly useful/progression
             item_classification = ItemClassification.filler
 
         return VagrantStoryItem(name, item_classification, VagrantStoryItem.get_name_to_id()[name], self.player)
 
     def get_filler_item_name(self) -> str:
-        return "Gil (50)" # this clearly needs looked into
-    
+        return "Gil (50)"  # this clearly needs looked into
+
     def set_rules(self) -> None:
-        
-        def is_level_cleared(self, location: str, state: CollectionState):        
-            return state.can_reach_location("Cleared: " + location, self.player)   
-        
-        def is_boss_defeated(self, boss: str, state: CollectionState): # can used later
+        def is_level_cleared(self, location: str, state: CollectionState):
+            return state.can_reach_location("Cleared: " + location, self.player)
+
+        def is_boss_defeated(self, boss: str, state: CollectionState):  # can used later
             return state.has("Boss: " + boss, self.player, 1)
-        
+
         def has_keyitems_required(self, items: list[str], state: CollectionState):
             passed_check = True
             for item in items:
-                if(state.has("Key Item: " + item, self.player, 1) is False):
+                if state.has("Key Item: " + item, self.player, 1) is False:
                     passed_check = False
             return passed_check
 
         for region in self.multiworld.get_regions(self.player):
             for location in region.locations:
-                    set_rule(location, lambda state: True)
-                    
-      
+                set_rule(location, lambda state: True)
+
         if self.options.goal.value == GoalOptions.DEFEAT_ANGEL:
             self.multiworld.completion_condition[self.player] = lambda state: state.can_reach_location("Level End: Credits", self.player)
         # Map rules
-        
+
         # ITEM SPECIFIC RULES
-        
+
         # for location in self.multiworld.get_locations(self.player):
         #     if location.parent_region.name in ["Dan's Crypt", "Locked Items DC"]:
         #         add_item_rule(location, lambda item: item.name != "Equipment: Hammer")
-        
+
         # options rule setup
-        
+
         # set_rule(self.get_entrance("Enchanted Earth -> Ant Hill"))
-        
+
         # Get a birds eye view of everything
-        
+
         # from Utils import visualize_regions
         # state = self.multiworld.get_all_state(False)
         # state.update_reachable_regions(self.player)
         # visualize_regions(self.get_region("Menu"), "medievil_layout.puml", show_entrance_names=True,
-        #                 regions_to_highlight=state.reachable_regions[self.player])        
-        
+        #                 regions_to_highlight=state.reachable_regions[self.player])
+
     def fill_slot_data(self) -> Dict[str, object]:
         slot_data: Dict[str, object] = {}
-
 
         name_to_vagrant_story_code = {item.name: item.v_code for item in item_dictionary.values()}
         # Create the mandatory lists to generate the player's output file
         items_id = []
         items_address = []
-        locations_id = [] 
+        locations_id = []
         locations_address = []
         locations_target = []
         for location in self.multiworld.get_filled_locations():
-
             if location.item is not None:
                 if location.item.player == self.player:
-                    #we are the receiver of the item
+                    # we are the receiver of the item
                     items_id.append(location.item.code)
                     items_address.append(name_to_vagrant_story_code[location.item.name])
 
-
             if location.player == self.player:
-                #we are the sender of the location check
+                # we are the sender of the location check
                 locations_address.append(item_dictionary[location_dictionary[location.name].default_item].v_code)
                 locations_id.append(location.address)
                 if location.item is not None:
@@ -322,7 +312,10 @@ class VagrantStoryWorld(World):
                 "guaranteed_items": self.options.guaranteed_items.value,
                 "goal": self.options.goal.value,
                 "deathlink": self.options.deathlink.value,
-                "progression_option": self.options.progression_option.value
+                "include_new_game_plus": self.options.include_new_game_plus.value,
+                "include_puzzle_mode_checks": self.options.include_puzzle_mode_checks,
+                "include_mid_boss_checks": self.options.include_mid_boss_checks.value,
+                "progression_option": self.options.progression_option.value,
             },
             "seed": self.multiworld.seed_name,  # to verify the server's multiworld
             "slot": self.multiworld.player_name[self.player],  # to connect to server
@@ -331,7 +324,7 @@ class VagrantStoryWorld(World):
             "locationsAddress": locations_address,
             "locationsTarget": locations_target,
             "itemsId": items_id,
-            "itemsAddress": items_address
+            "itemsAddress": items_address,
         }
 
         return slot_data
