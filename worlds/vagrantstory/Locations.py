b8c96ecf56e5aa1c595fc17bd4f66efb535bfede
riezahughes <dan@tantalus-troupe.com>
2026-03-15 15:28:17 +0100

    Adding Item Types, Removing print

--- worlds/vagrantstory/Locations.py
+++ worlds/vagrantstory/Locations.py
@@ -65,7 +65,7 @@ class VagrantStoryLocation(Location):
                 for j, location_data in enumerate(location_tables[region_name]):
                     # Assign an ID to each location within the region
                     # The ID for each location in a region will be current_region_base_id + j
-                    print(f"{current_region_base_id + j}: {location_data.name}")
+                    # print(f"{current_region_base_id + j}: {location_data.name}")
                     output[location_data.name] = current_region_base_id + j
 
         return output
